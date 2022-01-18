1321305165abd832f8f8a1d299f0d960f368e275
Cristian <cristian33244@gmail.com>
2022-01-18 15:54:48 -0500

    notificacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import { RestService } from './rest.service';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-push';
+  respuesta: any;
+
+  readonly VAPID_PUBLIC_KEY = "BBa2biKlbzt2xgurSQxMj_B7LzmEHlySrVYDFNb-HuWolC38Oovz1mlzOzBvp-YH-TYpqNf5ClTDFOSm1Xtx68Q";
+
+  constructor(private swPush: SwPush/*, private restService: RestService*/) {}
+
+  subscribeToNotifications() {
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    }).then(respuesta => {
+      this.respuesta = respuesta;
+    }).catch(err => {
+      this.respuesta = err;
+    });
+  }
 }
